bc1c49736bc316f724314db12acc39d94905eb5f
Marcin Ciapa <aaryndo@gmail.com>
2023-01-29 13:05:07 +0100

    Upgrade Spring Security to 6.0.1

---
+++ equino-security-spring-basic/src/main/java/ovh/equino/security/config/BasicAuthSecurity.java
@@ -0,0 +1,53 @@
+package ovh.equino.security.config;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import ovh.equino.security.crypto.PasswordEncoder;
+import ovh.equino.security.identity.IdentityRepository;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+public class BasicAuthSecurity {
+
+    private final HttpSecurity http;
+
+    public BasicAuthSecurity(
+            AuthenticationManagerBuilder auth,
+            HttpSecurity http,
+            IdentityRepository identityRepository,
+            PasswordEncoder passwordEncoder) {
+
+        this.http = http;
+
+        try {
+
+            auth
+                    .userDetailsService(new UserDetailsService(identityRepository))
+                    .passwordEncoder(passwordEncoder);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public SecurityFilterChain secure() {
+        try {
+
+            http
+                    .authorizeRequests().anyRequest().authenticated()
+                    .and()
+                    .httpBasic()
+                    .and()
+                    .csrf().disable()
+                    .cors()
+                    .and()
+                    .sessionManagement().sessionCreationPolicy(STATELESS);
+
+            return http.build();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
